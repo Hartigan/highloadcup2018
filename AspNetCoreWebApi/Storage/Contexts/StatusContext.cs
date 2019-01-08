9067f8138edd92f41e901ff0be81909a721c076e
Anatoly Safonov <hartigans@live.com>
2019-01-09 01:40:54 +0300

    Memory optimization: StatusContext

--- AspNetCoreWebApi/Storage/Contexts/StatusContext.cs
+++ AspNetCoreWebApi/Storage/Contexts/StatusContext.cs
@@ -3,56 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using AspNetCoreWebApi.Domain;
+using AspNetCoreWebApi.Processing;
 using AspNetCoreWebApi.Processing.Requests;
 
 namespace AspNetCoreWebApi.Storage.Contexts
 {
-    public class StatusContext
+    public class StatusContext : IBatchLoader<Status>
     {
         private ReaderWriterLock _rw = new ReaderWriterLock();
-        private Dictionary<Status, HashSet<int>> _id2AccId = new Dictionary<Status, HashSet<int>>();
+        private Dictionary<Status, List<int>> _id2AccId = new Dictionary<Status, List<int>>();
 
         public StatusContext()
         {
-            _id2AccId[Status.Complicated] = new HashSet<int>();
-            _id2AccId[Status.Free] = new HashSet<int>();
-            _id2AccId[Status.Reserved] = new HashSet<int>();
+            _id2AccId[Status.Complicated] = new List<int>();
+            _id2AccId[Status.Free] = new List<int>();
+            _id2AccId[Status.Reserved] = new List<int>();
+        }
+
+        public void LoadBatch(IEnumerable<BatchEntry<Status>> batch)
+        {
+            _rw.AcquireWriterLock(2000);
+            _id2AccId[Status.Complicated].AddRange(batch.Where(x => x.Value == Status.Complicated).Select(x => x.Id));
+            _id2AccId[Status.Free].AddRange(batch.Where(x => x.Value == Status.Free).Select(x => x.Id));
+            _id2AccId[Status.Reserved].AddRange(batch.Where(x => x.Value == Status.Reserved).Select(x => x.Id));
+            _id2AccId[Status.Complicated].Sort();
+            _id2AccId[Status.Free].Sort();
+            _id2AccId[Status.Reserved].Sort();
+            _rw.ReleaseWriterLock();
         }
 
         public void Add(int id, Status status)
         {
             _rw.AcquireWriterLock(2000);
-            _id2AccId[status].Add(id);
+            var list = _id2AccId[status];
+            list.Insert(~list.BinarySearch(id), id);
             _rw.ReleaseWriterLock();
         }
 
         public void Update(int id, Status status)
         {
             _rw.AcquireWriterLock(2000);
-            _id2AccId[Status.Complicated].Remove(id);
-            _id2AccId[Status.Free].Remove(id);
-            _id2AccId[Status.Reserved].Remove(id);
-            _id2AccId[status].Add(id);
+
+            var list = _id2AccId[status];
+
+            if (list.BinarySearch(id) < 0)
+            {
+                list.Insert(~list.BinarySearch(id), id);
+
+                foreach(var l in _id2AccId)
+                {
+                    if (l.Key == status)
+                    {
+                        continue;
+                    }
+
+                    int index = l.Value.BinarySearch(id);
+                    if (index >= 0)
+                    {
+                        l.Value.RemoveAt(index);
+                        break;
+                    }
+                }
+            }
             _rw.ReleaseWriterLock();
         }
 
         public Status Get(int id)
         {
-            _rw.AcquireReaderLock(2000);
-            
             Status status = Status.Complicated;
-            if (_id2AccId[Status.Free].Contains(id))
+            if (_id2AccId[Status.Free].BinarySearch(id) >= 0)
             {
                 status = Status.Free;
             }
 
-            if (_id2AccId[Status.Reserved].Contains(id))
+            if (_id2AccId[Status.Reserved].BinarySearch(id) >= 0)
             {
                 status = Status.Reserved;
             }
 
-
-            _rw.ReleaseReaderLock();
             return status;
         }
 
@@ -102,7 +130,7 @@ namespace AspNetCoreWebApi.Storage.Contexts
 
         public bool Contains(Status status, int id)
         {
-            return _id2AccId[status].Contains(id);
+            return _id2AccId[status].BinarySearch(id) >= 0;
         }
 
         public void GetByStatus(Status value, HashSet<int> currentIds)
