781d84919e68710d652c0e8267d9825ebd07fe2e
Anatoly Safonov <hartigans@live.com>
2019-01-09 00:34:14 +0300

    Memory optimization: FirstNameContext

--- AspNetCoreWebApi/Storage/Contexts/FirstNameContext.cs
+++ AspNetCoreWebApi/Storage/Contexts/FirstNameContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using AspNetCoreWebApi.Domain;
+using AspNetCoreWebApi.Processing;
 using AspNetCoreWebApi.Processing.Requests;
 
 namespace AspNetCoreWebApi.Storage.Contexts
@@ -10,20 +11,39 @@ namespace AspNetCoreWebApi.Storage.Contexts
     public class FirstNameContext
     {
         private ReaderWriterLock _rw = new ReaderWriterLock();
-        private SortedDictionary<int, string> _id2name = new SortedDictionary<int, string>();
+        private string[] _names = new string[DataConfig.MaxId];
+        private HashSet<int> _ids = new HashSet<int>();
 
         public FirstNameContext()
         {
         }
 
-        public void AddOrUpdate(int id, string name)
+        public void LoadBatch(IEnumerable<BatchEntry<string>> batch)
         {
             _rw.AcquireWriterLock(2000);
-            _id2name[id] = string.Intern(name);
+            foreach(var entry in batch)
+            {
+                _names[entry.Id] = entry.Value;
+                _ids.Add(entry.Id);
+            }
             _rw.ReleaseWriterLock();
         }
 
-        public bool TryGet(int id, out string fname) => _id2name.TryGetValue(id, out fname);
+        public void AddOrUpdate(int id, string name)
+        {
+            _rw.AcquireWriterLock(2000);
+
+            _names[id] = string.Intern(name);
+            _ids.Add(id);
+
+            _rw.ReleaseWriterLock();
+        }
+
+        public bool TryGet(int id, out string fname)
+        {
+            fname = _names[id];
+            return fname != null;
+        }
 
         public IEnumerable<int> Filter(
             FilterRequest.FnameRequest fname,
@@ -35,7 +55,7 @@ namespace AspNetCoreWebApi.Storage.Contexts
                 {
                     if (fname.Eq == null && fname.Any == null)
                     {
-                        return idStorage.Except(_id2name.Keys);
+                        return idStorage.Except(_ids);
                     }
                     else
                     {
@@ -48,7 +68,7 @@ namespace AspNetCoreWebApi.Storage.Contexts
             {
                 if (fname.Any.Contains(fname.Eq))
                 {
-                    return _id2name.Where(x => x.Value == fname.Eq).Select(x => x.Key);
+                    return _ids.Where(x => _names[x] == fname.Eq);
                 }
                 else
                 {
@@ -59,14 +79,14 @@ namespace AspNetCoreWebApi.Storage.Contexts
             if (fname.Any != null)
             {
                 HashSet<string> names = new HashSet<string>(fname.Any);
-                return _id2name.Where(x => names.Contains(x.Value)).Select(x => x.Key);
+                return _ids.Where(x => names.Contains(_names[x]));
             }
             else if (fname.Eq != null)
             {
-                return _id2name.Where(x => x.Value == fname.Eq).Select(x => x.Key);
+                return _ids.Where(x => _names[x] == fname.Eq);
             }
 
-            return _id2name.Keys;
+            return _ids;
         }
     }
 }
