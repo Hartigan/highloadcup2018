681932429e23d28c9ad8bece36a4c97f8b45a171
Anatoly Safonov <hartigans@live.com>
2019-01-09 01:35:32 +0300

    Memory optimization: PremiumContext

--- AspNetCoreWebApi/Storage/Contexts/PremiumContext.cs
+++ AspNetCoreWebApi/Storage/Contexts/PremiumContext.cs
@@ -8,36 +8,82 @@ using AspNetCoreWebApi.Processing.Requests;
 
 namespace AspNetCoreWebApi.Storage.Contexts
 {
-    public class PremiumContext
+    public class PremiumContext : IBatchLoader<Premium>
     {
         private ReaderWriterLock _rw = new ReaderWriterLock();
-        private SortedDictionary<int, Premium> _id2item = new SortedDictionary<int, Premium>();
+        private Premium?[] _premiums = new Premium?[DataConfig.MaxId];
         private HashSet<int> _now = new HashSet<int>();
+        private HashSet<int> _ids = new HashSet<int>();
 
 
         public PremiumContext()
         {
         }
 
-        public void AddOrUpdate(int id, Premium item)
+        public void LoadBatch(IEnumerable<BatchEntry<Premium>> batch)
         {
             _rw.AcquireWriterLock(2000);
-            if (_id2item.ContainsKey(id))
+
+            foreach(var entry in batch)
             {
-                _id2item.Remove(id);
-                _now.Remove(id);
+                _premiums[entry.Id] = entry.Value;
+                _ids.Add(entry.Id);
+                if (entry.Value.IsNow())
+                {
+                    _now.Add(entry.Id);
+                }
             }
 
-            _id2item[id] = item;
-            if (item.IsNow())
-            {
-                _now.Add(id);
-            }
-            
             _rw.ReleaseWriterLock();
         }
 
-        public bool TryGet(int id, out Premium premium) => _id2item.TryGetValue(id, out premium);
+        public void AddOrUpdate(int id, Premium item)
+        {
+            _rw.AcquireWriterLock(2000);
+
+            var prev = _premiums[id];
+
+            if (prev.HasValue)
+            {
+                if (prev.Value.IsNow() != item.IsNow())
+                {
+                    if (prev.Value.IsNow())
+                    {
+                        _now.Remove(id);
+                    }
+                    else
+                    {
+                        _now.Add(id);
+                    }
+                }
+            }
+            else
+            {
+                _ids.Add(id);
+                if (item.IsNow())
+                {
+                    _now.Add(id);
+                }
+            }
+
+            _premiums[id] = item;
+
+            _rw.ReleaseWriterLock();
+        }
+
+        public bool TryGet(int id, out Premium premium)
+        {
+            if (_premiums[id].HasValue)
+            {
+                premium = _premiums[id].Value;
+                return true;
+            }
+            else
+            {
+                premium = default(Premium);
+                return false;
+            }
+        }
 
         public IEnumerable<int> Filter(
             FilterRequest.PremiumRequest premium,
@@ -47,7 +93,7 @@ namespace AspNetCoreWebApi.Storage.Contexts
             {
                 if (premium.IsNull.Value)
                 {
-                    return premium.Now ? Enumerable.Empty<int>() : ids.Except(_id2item.Keys);
+                    return premium.Now ? Enumerable.Empty<int>() : ids.Except(_ids);
                 }
             }
 
@@ -57,10 +103,10 @@ namespace AspNetCoreWebApi.Storage.Contexts
             }
             else
             {
-                return _id2item.Keys;
+                return _ids;
             }
         }
 
-        public bool IsNow(int x) => _now.Contains(x);
+        public bool IsNow(int id) => _now.Contains(id);
     }
 }
