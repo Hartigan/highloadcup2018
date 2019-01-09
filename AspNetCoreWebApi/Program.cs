504aabe259cfd4b4a1c4045a6e15cc5f2214b4ed
Anatoly Safonov <hartigans@live.com>
2019-01-09 03:14:14 +0300

    Open server to any ip

--- AspNetCoreWebApi/Program.cs
+++ AspNetCoreWebApi/Program.cs
@@ -36,7 +36,7 @@ namespace AspNetCoreWebApi
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseKestrel(options => {
-                    options.Listen(IPAddress.Loopback, 8080);
+                    options.Listen(IPAddress.Any, 80);
                 })
                 .UseStartup<Startup>();
     }
